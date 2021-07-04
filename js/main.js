062bd2859437017c249691b48949408bea4e9b3f
Hiroo0121 <86660605+Hiroo0121@users.noreply.github.com>
2021-07-04 16:27:38 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+  $('.slider').slick({
+  	speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 500,
+    slidesToShow: 3,
+    infinite: true,
+    accessibility: true,
+    pauseOnFocus: true,
+  });
